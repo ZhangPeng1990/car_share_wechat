f1b820f354f294a2c72d7a98760f6cc305a1b2a7
zp <ZP151189@163.com>
2017-07-15 23:18:30 +0800

    first commiy

---
+++ src/main/java/car/top/content360/CarShareWechatApplication.java
@@ -0,0 +1,12 @@
+package car.top.content360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShareWechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarShareWechatApplication.class, args);
+	}
+}
